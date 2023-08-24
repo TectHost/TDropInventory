bc87877392cea5cbeecfab456242384db09ef82b
Tect.host <tect.host@gmail.com>
2023-08-24 21:58:58 +0200

    Add files via upload

--- src/minealex/tdropinventory/commands/Commands.java
+++ src/minealex/tdropinventory/commands/Commands.java
@@ -1,13 +1,14 @@
 package minealex.tdropinventory.commands;
 
 import minealex.tdropinventory.TDropInventory;
+
+import java.io.File;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-
-import java.io.File;
 
 public class Commands implements CommandExecutor {
 
@@ -19,6 +20,20 @@ public class Commands implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 1) {
+            if (args[0].equalsIgnoreCase("reload")) {
+                return handleReloadCommand(sender);
+            } else if (args[0].equalsIgnoreCase("version")) {
+                return handleVersionCommand(sender);
+            }
+        }
+
+        // Si se proporcionan argumentos no válidos o ninguno, muestra un mensaje de ayuda o error.
+        sender.sendMessage(ChatColor.RED + "Invalid command. Correct usage: /tdi reload or /tdi version");
+        return true;
+    }
+
+    private boolean handleReloadCommand(CommandSender sender) {
         if (!sender.hasPermission("tdi.reload")) {
             String noPermissionMessage = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.no-permission"));
             sender.sendMessage(noPermissionMessage);
@@ -41,6 +56,20 @@ public class Commands implements CommandExecutor {
         String reloadMessage = ChatColor.translateAlternateColorCodes('&', config.getString("messages.reload"));
         sender.sendMessage(reloadMessage);
 
+        return true;
+    }
+
+    private boolean handleVersionCommand(CommandSender sender) {
+        if (!sender.hasPermission("tdi.version")) {
+            String noPermissionMessage = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.no-permission"));
+            sender.sendMessage(noPermissionMessage);
+            return true;
+        }
+
+        String versionMessage = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.version"));
+        versionMessage = versionMessage.replace("%version%", plugin.getDescription().getVersion());
+
+        sender.sendMessage(versionMessage);
         return true;
     }
 }
